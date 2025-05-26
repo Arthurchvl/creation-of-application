b6f8025bbfbaad031dea1922f7d24eda020d030c
Arthur Chauvel <arthurchauvel207@gmail.com>
2025-05-26 16:57:16 +0200

    remise fichiers propres

---
+++ PlanTable.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class PlanTable {
+    private ArrayList<Table> planTable;
+
+    public PlanTable(ArrayList<Table> planTable) {
+        this.planTable = planTable;
+    }
+
+    public ArrayList<Table> getPlanTable() {
+        return planTable;
+    }
+
+    public void setPlanTable(ArrayList<Table> planTable) {
+        this.planTable = planTable;
+    }
+
+}
--- code/PlanTable.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-
-public class PlanTable {
-    private ArrayList<Table> = new ArrayList<>;
-    
-}
