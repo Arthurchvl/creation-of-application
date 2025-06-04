e0bd259d8aa25f06b588826bffbe2019b1f83b25
Timehwauuwww <lemoingtimeo@gmail.com>
2025-06-04 15:54:05 +0200

    supprimer

---
+++ Controleur/CtrlSupprimerPersonne.java
@@ -0,0 +1,29 @@
+package vue;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CtrlSupprimerPersonne {
+
+    @FXML
+    private Button bnAnnuler;
+
+    @FXML
+    private Label lblNom;
+
+    @FXML
+    private Label lblPrenom;
+
+    @FXML
+    void retourFenAccueil(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retourFenInfoPersonne(ActionEvent event) {
+
+    }
+
+}
